6bf4a13ecfd2df42924c9c0521c70496f7b41978
HubertHuckevoll <konstantin.meyer@gmail.com>
2022-11-06 21:40:25 +0100

    reworking controllers to be ES6 module

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
 import * as http        from 'http';
-import { OverviewC }    from './_c/OverviewC.js';
-import { FeedContentC } from './_c/FeedContentC.js';
-import { PreviewC }     from './_c/PreviewC.js';
-import { ImageProxyC }  from './_c/ImageProxyC.js';
-import { PassthroughC } from './_c/PassthroughC.js';
-import { EmptyC }       from './_c/EmptyC.js';
-import { Html3V }       from './_v/Html3V.js';
 import { TsvImp }       from './_l/TsvImp.js';
 import * as tools       from './_l/Tools.js';
+import * as cntrl       from './_c/Control.js';
+import { Html3V }       from './_v/Html3V.js';
 
 class App
 {
@@ -16,13 +11,7 @@ class App
     this.pAdress = 'http://localhost:'+port.toString()+'/';
     this.rssHintTableAddress = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTToY09sxeo57zbb-5hXF7ElwI6NaDACTWx_itnF4yVV9j1V_s-H3FTCKP8a17K22tzLFazhCcO82uL/pub?output=tsv';
     this.rssHintTable = null;
-
     this.view = new Html3V();
-    this.feedContentC = new FeedContentC(this.view);
-    this.imageProxyC = new ImageProxyC();
-    this.previewC = new PreviewC(this.view);
-    this.passthroughC = new PassthroughC(this.view);
-    this.emptyC = new EmptyC();
   }
 
   async init()
@@ -32,8 +21,6 @@ class App
     {
       const rawTable = await res.text();
       this.rssHintTable = new TsvImp().fromTSV(rawTable);
-      this.overviewC = new OverviewC(this.view, this.rssHintTable);
-
     }
   }
 
@@ -54,42 +41,42 @@ class App
       // passthrough
       if (url.includes('geos-infobase')) //FIXME: add mode for viewing original page
       {
-        wasProcessed = await this.passthroughC.get(request, response, url);
+        wasProcessed = await cntrl.passthroughC(request, response, url);
       }
 
       // is image - proxy image, convert to to GIF
       if ((wasProcessed === false) &&
           (await tools.isImage(url)))
       {
-        wasProcessed = await this.imageProxyC.get(response, url);
+        wasProcessed = await cntrl.imageProxyC(response, url);
       }
 
       // is RSS - show feed content
       if ((wasProcessed === false) &&
           (await tools.isRss(url)))
       {
-        wasProcessed = await this.feedContentC.get(response, url);
+        wasProcessed = await cntrl.feedContentC(this.view, response, url);
       }
 
       // is "homepage" - show overwiew
       if ((wasProcessed === false) &&
           (url == tld))
       {
-        wasProcessed = await this.overviewC.get(response, url);
+        wasProcessed = await cntrl.overviewC(this.view, this.rssHintTable, response, url);
       }
 
       // referer is RSS - show article extract
       if ((wasProcessed === false) &&
           (await tools.isRss(referer)))
       {
-        wasProcessed = await this.previewC.get(response, url);
+        wasProcessed = await cntrl.previewC(this.view, response, url);
       }
     }
 
     // is something else: return empty (works best...)
     if (wasProcessed === false)
     {
-      wasProcessed = this.emptyC.get(response);
+      wasProcessed = cntrl.emptyC(response);
     }
   }
 }
