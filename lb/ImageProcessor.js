b7b9ac13142d5539ab8a0476e069bb7bb3158aa2
Konstantin Meyer <konstantin.meyer@gmail.com>
2023-11-09 23:27:02 +0100

    remove svg2img as sharp does this

--- lb/ImageProcessor.js
+++ lb/ImageProcessor.js
@@ -1,6 +1,5 @@
 import * as tools from '../lb/Tools.js';
 import imgManip from 'sharp';
-import svg2img  from 'svg2img';
 
 export class ImageProcessor
 {
@@ -15,23 +14,7 @@ export class ImageProcessor
     {
       let bin = null;
       let imgBuffer = await tools.rFetch(url);
-
-      if (mimeType == 'image/svg+xml')
-      {
-        imgBuffer = await imgBuffer.text();
-        imgBuffer = await new Promise(function (resolve, reject)
-        {
-          svg2img(imgBuffer, function(error, buffer)
-          {
-            if (error) reject();
-            resolve(buffer);
-          });
-        })
-      }
-      else
-      {
-        imgBuffer = await imgBuffer.arrayBuffer();
-      }
+      imgBuffer = await imgBuffer.arrayBuffer();
 
       const data = await imgManip(imgBuffer).metadata();
       const w = data.width;
